148418066c443196975094e7578c4e10682ce48e
wxf907 <wxfei66@gmail.com>
2026-07-07 18:21:04 +0800

    Implement fetch function to proxy requests

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request, env, ctx) {
+    const targetUrl = 'https://zq8p5xnqrq.coze.site'
+    const url = new URL(request.url)
+    url.hostname = new URL(targetUrl).hostname
+    
+    const modifiedRequest = new Request(url, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body
+    })
+    
+    return fetch(modifiedRequest)
+  }
+}
