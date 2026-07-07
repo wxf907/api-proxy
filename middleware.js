3f0c2f12145aa3dfbb01d566259400fb26dc2281
wxf907 <wxfei66@gmail.com>
2026-07-07 20:28:37 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export function middleware(context) {
   const { request, rewrite } = context;
-  // https://75012b38-7e1a-468f-a9ef-65ac1307ffa0.dev.coze.site
-  return rewrite('https://75012b38-7e1a-468f-a9ef-65ac1307ffa0.dev.coze.site' + new URL(request.url).pathname);
+  const url = new URL(request.url);
+  return rewrite('https://75012b38-7e1a-468f-a9ef-65ac1307ffa0.dev.coze.site' + url.pathname + url.search);
 }
