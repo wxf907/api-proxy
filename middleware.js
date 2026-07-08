2c80981e8b395cca09511ff9c44684480fc3af64
wxf907 <wxfei66@gmail.com>
2026-07-08 11:33:29 +0800

    Update middleware URL for request rewriting

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export function middleware(context) {
   const { request, rewrite } = context;
   const url = new URL(request.url);
-  return rewrite('https://75012b38-7e1a-468f-a9ef-65ac1307ffa0.dev.coze.site' + url.pathname + url.search);
+  return rewrite('https://7b1a24bf.r15.cpolar.top' + url.pathname + url.search);
 }
